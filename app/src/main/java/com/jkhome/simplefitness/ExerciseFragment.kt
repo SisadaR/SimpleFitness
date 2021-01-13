d94965c8770a75117df655549ed1a1a9f7e17e0c
Sisada R <kong.sisada@gmail.com>
2021-01-14 03:33:58 +0700

    add, cycle through exercises

--- app/src/main/java/com/jkhome/simplefitness/ExerciseFragment.kt
+++ app/src/main/java/com/jkhome/simplefitness/ExerciseFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.jkhome.simplefitness.databinding.FragmentExerciseBinding
 
 //// TODO: Rename parameter arguments, choose names that match
 //// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +20,69 @@ import android.view.ViewGroup
 // * create an instance of this fragment.
 // */
 class ExerciseFragment : Fragment() {
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+
+
+    data class Exercise(
+        val exerciseType: String,
+        val exerciseCount: Int
+    )
+
+    private val exercises: MutableList<Exercise> = mutableListOf(
+        Exercise("gifone",5),
+        Exercise("giftwo",9),
+        Exercise("gifthree",4),
+        Exercise("giffour",6),
+        Exercise("giffive",8)
+    )
+
+    private val binding: FragmentExerciseBinding by lazy { FragmentExerciseBinding.inflate(layoutInflater) }
+    private lateinit var currentExercise: Exercise
+    private var exerciseIndex : Int = 0
+    private var exerciseSize = exercises.size / 2
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false)
+
+        randomExercise()
+        binding.nextButton.setOnClickListener { view:View ->
+            exerciseIndex++
+            if (exerciseIndex < exerciseSize) {
+                currentExercise = exercises[exerciseIndex]
+                setExercise()
+            } else {
+
+            }
+        }
+
+        return binding.root
+
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment ExerciseFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            ExerciseFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    private fun randomExercise() {
+        exercises.shuffle()
+        exerciseIndex = 0
+        setExercise()
+    }
+
+    private fun setExercise() {
+        currentExercise = exercises[exerciseIndex]
+        binding.exerciseTextView.text = String.format("%d Time", currentExercise.exerciseCount)
+        binding.exerciseImageView.setImageResource(
+            resources.getIdentifier(currentExercise.exerciseType,
+            "drawable",
+            (activity as AppCompatActivity).packageName))
+
+        (activity as AppCompatActivity).supportActionBar?.title =
+            String.format("%d/%d",
+                exerciseIndex + 1,
+                exerciseSize )
+
+
+
+    }
+
+
 }
--- app/src/main/java/com/jkhome/simplefitness/MainActivity.kt
+++ app/src/main/java/com/jkhome/simplefitness/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
