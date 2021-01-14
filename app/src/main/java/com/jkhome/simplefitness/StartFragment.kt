efb4d14831e36225714f602d04c70eb325f1a8ca
Sisada R <kong.sisada@gmail.com>
2021-01-15 03:10:38 +0700

    update, fix commit

--- app/src/main/java/com/jkhome/simplefitness/StartFragment.kt
+++ app/src/main/java/com/jkhome/simplefitness/StartFragment.kt
@@ -1,12 +1,12 @@
 package com.jkhome.simplefitness
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class StartFragment : Fragment() {
 
@@ -24,8 +24,21 @@ class StartFragment : Fragment() {
         goButton.setOnClickListener { view:View ->
             Navigation.findNavController(view).navigate(R.id.action_startFragment_to_exerciseFragment)
         }
+
+        setHasOptionsMenu(true)
+
         return v
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            requireView().findNavController()) ||
+                super.onOptionsItemSelected(item)
+    }
 }
